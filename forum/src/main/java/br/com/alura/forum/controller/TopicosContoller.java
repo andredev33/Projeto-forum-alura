66ddc058361880cee29131caa9b0f6759782a52a
Andre Silva <andre.silva.dev2020@gmail.com>
2021-10-07 17:09:56 -0300

    Configurção do JPA

--- forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
@@ -16,9 +17,9 @@ public class TopicosContoller {
 	
 	@RequestMapping("/topicos")
 	
-	public List<Topico> list(){
+	public List<TopicoDto> list(){
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring-boot", "Programação"));
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 
 }
