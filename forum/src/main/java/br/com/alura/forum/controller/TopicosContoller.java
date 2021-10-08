e1a42092aef42a587cf7ca0be773143b6c35d882
Andre Silva <andre.silva.dev2020@gmail.com>
2021-10-08 15:49:55 -0300

    Spring Data JPA e criação de um Repository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosContoller.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosContoller {
 	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	
 	public List<TopicoDto> list(){
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring-boot", "Programação"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 
 }
